9b53fca85708776bfb7c6cd9e00d4a0595e774b1
MehfCodes <mrmohammadehf@gmail.com>
2021-05-30 03:27:25 +0430

    modify 'Special Pythagorean triplet.py' and 'Highly divisible triangular number.py'

--- Highly divisible triangular number.py
+++ Highly divisible triangular number.py
@@ -4,10 +4,10 @@ from sympy import factorint
 def number_of_divisors(n):
     prime_factors = factorint(n)
     mul = 1
-    print(prime_factors)
+    # print(prime_factors)
     for i in prime_factors.values():
         mul *= i+1
-        print(mul)
+        # print(mul)
     return mul
 
 
@@ -15,7 +15,7 @@ def generate_triangle_number():
     n = 1
     while True:
         t = (n*(n+1))//2
-        if number_of_divisors(t) > 5:
+        if number_of_divisors(t) > 500:
             return t
         else:
             n += 1
--- Special Pythagorean triplet.py
+++ Special Pythagorean triplet.py
@@ -2,62 +2,112 @@
 # import datetime
 
 
-def sumPrimes(n):
-    sum, sieve = 0, [True] * n
-    for p in range(2, n):
-        if sieve[p]:
-            sum += p
-            for i in range(p*p, n, p):
-                sieve[i] = False
+# # def sumPrimes(n):
+# #     sum, sieve = 0, [True] * n
+# #     for p in range(2, n):
+# #         if sieve[p]:
+# #             sum += p
+# #             for i in range(p*p, n, p):
+# #                 sieve[i] = False
+# #     return sum
+
+
+# # print(sumPrimes(2000000))
+
+# # start = datetime.datetime.now()
+# # # my code
+
+
+# # def is_prime(x):
+# #     for i in range(2, int(sqrt(x)+1)):
+# #         if x % i == 0:
+# #             return False
+# #     return True
+
+
+# # def sum_of_pn_below(n):
+# #     sum = 0
+# #     for i in range(2, n):
+# #         if is_prime(i):
+# #             sum += i
+# #     return sum
+
+
+# # print(sum_of_pn_below(2000000))
+# # end = datetime.datetime.now()
+
+
+# # print(end - start)
+# for a in range(1, 1000):
+#     for b in range(1, 1000):
+#         if a < b:
+#             for c in range(1, 1000):
+#                 if b < c:
+#                     result = a+b+c
+#                     if result == 1000:
+#                         a2 = pow(a, 2)
+#                         b2 = pow(b, 2)
+#                         c2 = pow(c, 2)
+#                         if a2+b2 == c2:
+#                             abc = a*b*c
+#                             print(abc)
+#                 else:
+#                     continue
+#         else:
+#             continue
+
+
+# def is_dev_pytha(a, b, c):
+#     if pow(a, 2)+pow(b, 2) == pow(c, 2):
+#         return True
+#     else:
+#         False
+
+from math import sqrt, pow
+
+# my code
+
+
+def sum_of_pn_below(n):
+    # stack = []
+    sum = 0
+    for i in range(2, n+1):
+        counter = 0
+        for k in range(2, int(sqrt(i))):
+            if i % k == 0:
+                counter += 1
+        if counter == 0:
+            sum += i
+    #         print(i)
+    #         stack.append(i)
+    # for x in stack:
+    #     sum += x
     return sum
 
 
-print(sumPrimes(2000000))
-# start = datetime.datetime.now()
-# # my code
+print(sum_of_pn_below(2000000))
+
+for a in range(1, 1000):
+    for b in range(1, 1000):
+        if a < b:
+            for c in range(1, 1000):
+                if b < c:
+                    result = a+b+c
+                    if result == 1000:
+                        a2 = pow(a, 2)
+                        b2 = pow(b, 2)
+                        c2 = pow(c, 2)
+                        if a2+b2 == c2:
+                            abc = a*b*c
+                            print(abc)
+                else:
+                    continue
+        else:
+            continue
 
 
-# def is_prime(x):
-#     for i in range(2, int(sqrt(x)+1)):
-#         if x % i == 0:
-#             return False
-#     return True
-
-
-# def sum_of_pn_below(n):
-#     sum = 0
-#     for i in range(2, n):
-#         if is_prime(i):
-#             sum += i
-#     return sum
-
-
-# print(sum_of_pn_below(2000000))
-# end = datetime.datetime.now()
-
-
-# print(end - start)
-# # for a in range(1, 1000):
-# #     for b in range(1, 1000):
-# #         if a < b:
-# #             for c in range(1, 1000):
-# #                 if b < c:
-# #                     result = a+b+c
-# #                     if result == 1000:
-# #                         a2 = pow(a, 2)
-# #                         b2 = pow(b, 2)
-# #                         c2 = pow(c, 2)
-# #                         if a2+b2 == c2:
-# #                             abc = a*b*c
-# #                             print(abc)
-# #                 else:
-# #                     continue
-# #         else:
-# #             continue
-
-
-# # def is_dev_pytha(a, b, c):
-# #     if pow(a, 2)+pow(b, 2) == pow(c, 2):
-# #         return True
-# #     else:
-# #         False
+def is_dev_pytha(a, b, c):
+    if pow(a, 2)+pow(b, 2) == pow(c, 2):
+        return True
+    else:
+        False
